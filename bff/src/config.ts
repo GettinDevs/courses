a737876a0fc37e88430f22ff98e68812ce3577f4
tomastvmv <tomastvmv@gmail.com>
2022-12-04 22:22:20 +0000

    omit file extensions on esm

---
+++ bff/src/config.ts
@@ -0,0 +1,8 @@
+export const config = {
+  app: {
+    port: process.env["PORT"] || 8000,
+  },
+  db: {
+
+  }
+};
--- bff/src/index.ts
+++ bff/src/index.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 import cors from 'cors'
 
+import { config } from './config';
+
 const app = express();
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+app.get('/api', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(config.app.port, () => {
+  console.log(`Server listening on port ${config.app.port}`);
 })
