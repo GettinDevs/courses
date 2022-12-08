049eb429f219c9904f59b3741478744f7dc29988
tomastvmv <tomastvmv@gmail.com>
2022-12-08 03:48:19 +0000

    add client build - DO instance too low on ram

---
+++ client/build/static/js/6781.12fe68b4.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkclient=self.webpackChunkclient||[]).push([[6781],{6781:function(e,n,r){r.r(n),r.d(n,{diff:function(){return i}});var t={"+":"inserted","-":"deleted","@":"meta"},i={name:"diff",token:function(e){var n=e.string.search(/[\t ]+?$/);if(!e.sol()||0===n)return e.skipToEnd(),("error "+(t[e.string.charAt(0)]||"")).replace(/ $/,"");var r=t[e.peek()]||e.skipToEnd();return-1===n?e.skipToEnd():e.pos=n,r}}}}]);
+//# sourceMappingURL=6781.12fe68b4.chunk.js.map
